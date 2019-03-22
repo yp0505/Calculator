3b31193c2b4b7279b661b6f37a712b72d75b1510
Yash <Yash@MSI>
2019-03-22 12:08:22 +0530

    hi

--- src/main/java/main/Calculator.java
+++ src/main/java/main/Calculator.java
@@ -39,6 +39,7 @@ public class Calculator {
     {
     	int result=0;
 //        write your code here 
+    	result = x+y;
     	
         return result;
     }
@@ -46,18 +47,21 @@ public class Calculator {
     {
     	int result=0;
 //        write your code here
+    	result = x-y;
         return result;
     }
     public static int mult(int x, int y)
     {
     	int result=0;
-//      write your code here        
+//      write your code here
+    	result = x*y;
     	return result;
     }
     public static int div(int x, int y)
     {
     	int result=0;
 //      write your code here
+    	result = x/y;
     	return result;
     }
 
